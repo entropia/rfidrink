e0d8ba3f8b51ffc4e1fa687f82a438058c7a960f
Emmanouil Kampitakis <info+git@kampitakis.de>
2016-07-03 22:46:21 +0000

    Scipt now runs in an infinite loop and simulates keyboard input

--- rfidrink.py
+++ rfidrink.py
@@ -1,22 +1,25 @@
-#!/usr/bin/python3
-from smartcard.CardMonitoring import CardMonitor, CardObserver
+#! /usr/bin/env python
+from pykeyboard import PyKeyboard
+from smartcard.CardType import AnyCardType
+from smartcard.CardRequest import CardRequest
 from smartcard.util import toHexString
 
-from time import sleep
+while True:
+    try:
+        # define the apdus used in this script
+        SELECT = [0xFF, 0xCA, 0x00, 0x00, 0x00]
 
-class PrintObserver(CardObserver):
-    def update(self, observable, actions):
-        (addedcards, removedcards) = actions
-        for card in addedcards:
-            print("+Inserted: ", toHexString(card.atr))
-        for card in removedcards:
-            print("-Removed: ", toHexString(card.atr))
+        # request card insertion
+        cardrequest = CardRequest(timeout=None, cardType=AnyCardType(), newcardonly=True)
+        cardservice = cardrequest.waitforcard()
 
-if __name__ == '__main__':
-    cardmonitor = CardMonitor()
-    cardobserver = PrintObserver()
-    cardmonitor.addObserver(cardobserver)
+        # connect to the card and perform a few transmits
+        cardservice.connection.connect()
+        apdu = SELECT
+        response, sw1, sw2 = cardservice.connection.transmit(apdu)
+        rfid_string = '_'.join(map(str, response))
 
-    sleep(10)
-
-    cardmonitor.deleteObserver(cardobserver)
+        keyboard = PyKeyboard()
+        keyboard.type_string('RFID_ID_'+rfid_string)
+    except:
+        continue
