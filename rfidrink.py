entropia/rfidrink

commit cf809c481366a0b7fbde6777370e932275283a74

Fixes #1: Added a press of thrice the ESC character

rfidrink.py
@@ -26,7 +26,8 @@ def main():
             rfid_string = '_'.join([str(id_val) for id_val in response])
 
             keyboard = PyKeyboard()
-            keyboard.type_string('<U>RFID_ID_'+rfid_string)
+            keyboard.press_key(keyboard.escape_key, n=3, interval=0.05)
+            keyboard.type_string('<U>RFID_ID_'rfid_string)
         except:
             continue
 

commit 57d9140bbf8c58b65955f438cab4b28c68a3e2d7

Fixed typo in keyboard press string introduced with cf809c481366a0b7fbde6777370e932275283a74

rfidrink.py
@@ -27,7 +27,7 @@ def main():
 
             keyboard = PyKeyboard()
             keyboard.press_key(keyboard.escape_key, n=3, interval=0.05)
-            keyboard.type_string('<U>RFID_ID_'rfid_string)
+            keyboard.type_string('<U>RFID_ID_'+rfid_string)
         except:
             continue
 